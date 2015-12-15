7b8a426bab1cfde9cf68be985efd7adf0b063628
fanuel <fanuelson@gmail.com>
2015-12-15 01:25:32 -0200

    Concertando classe vazia.

--- src/main/java/com/snf/controller/HomeController.java
+++ src/main/java/com/snf/controller/HomeController.java
@@ -1,10 +1,6 @@
 package com.snf.controller;
 
-import java.util.List;
-
-import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
