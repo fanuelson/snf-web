aa75f2935b43ed2a8d89eac19c05b8b68ce1a13b
fanuel nunes <fanuelson@gmail.com>
2016-01-11 21:55:50 -0200

    Pequenas Refatoraçoes...

--- src/main/java/teste/Teste1.java
+++ src/main/java/teste/Teste1.java
@@ -4,16 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+@SuppressWarnings("unused")
 public class Teste1 {
 	
-	
-
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("restauranteCDI");
 		EntityManager entityManager = emf.createEntityManager();
-		
-		
-			
 			
 	}
 
