353dd4048c1df80d58e9048adf44daa3a2b939b9
fanuel nunes <fanuelson@gmail.com>
2016-03-19 03:50:42 -0300

    Inserir mais métodos utilitários de data.

--- src/main/java/com/snf/util/DataUtil.java
+++ src/main/java/com/snf/util/DataUtil.java
@@ -25,5 +25,21 @@ public class DataUtil {
 	public static DateTime getDateTimeHoraZerada(){
 		return new DateTime(new DateTime().getYear(),new DateTime().getMonthOfYear(),new DateTime().getDayOfMonth(),0,0);
 	}
+	
+	public static Date getDataAtualHoraZerada() {
+		DateTime dt = new DateTime(new Date()).withTimeAtStartOfDay();
+		return dt.toDate();
+	}
+	
+	public static Date getDataAtualHoraFinalDia() {
+		DateTime dt = new DateTime(new Date()).withTimeAtStartOfDay()
+				.plusDays(1)
+				.minusSeconds(1);
+		return dt.toDate();
+	}
+	
+	public static String getDataFormatada(Date data, String formato) {
+		return new DateTime(data).toString(formato);
+	}
 
 }
