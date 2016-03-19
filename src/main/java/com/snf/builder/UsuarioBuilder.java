250b8f93c88fd8e7365f19c66d6526bf6da1bc61
fanuel nunes <fanuelson@gmail.com>
2016-03-18 21:05:08 -0300

    Criar builder para Usuário.

---
+++ src/main/java/com/snf/builder/UsuarioBuilder.java
@@ -0,0 +1,36 @@
+package com.snf.builder;
+
+import com.snf.enums.TipoUsuario;
+import com.snf.model.Usuario;
+
+public class UsuarioBuilder {
+
+	private Usuario usuario;
+	
+	public UsuarioBuilder() {
+		usuario = new Usuario();
+	}
+	
+	public UsuarioBuilder comNome(String nome) {
+		usuario.setNome(nome);
+		return this;
+	}
+	
+	public UsuarioBuilder comLogin(String login) {
+		usuario.setLogin(login);
+		return this;
+	}
+	public UsuarioBuilder comSenha(String senha) {
+		usuario.setSenha(senha);
+		return this;
+	}
+	
+	public UsuarioBuilder doTipo(TipoUsuario tipo) {
+		usuario.setTipo(tipo);
+		return this;
+	}
+	
+	public Usuario contruir() {
+		return this.usuario;
+	}
+}
