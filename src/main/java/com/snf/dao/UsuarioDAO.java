a4ebfffc2314788a76459b419629d4657bbe86f0
fanuel nunes <fanuelson@gmail.com>
2016-03-28 19:19:29 -0300

    Retirar idéia de múltiplos bancos.

--- src/main/java/com/snf/dao/UsuarioDAO.java
+++ src/main/java/com/snf/dao/UsuarioDAO.java
@@ -1,10 +1,7 @@
 package com.snf.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.snf.model.Usuario;
 
-@Repository
 public class UsuarioDAO extends GenericDAO<Usuario, Long> {
 
 	private static final long serialVersionUID = 5201746283603277625L;
--- src/main/java/com/snf/persistence/JPAUtil.java
+++ src/main/java/com/snf/persistence/JPAUtil.java
@@ -1,23 +1,18 @@
 package com.snf.persistence;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.log4j.Logger;
 
-import com.snf.controller.CommonsController;
-
-@SessionScoped
+@ApplicationScoped
 public class JPAUtil implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -26,9 +21,6 @@ public class JPAUtil implements Serializable {
 
 	private EntityManagerFactory factory;
 	
-	@Inject
-	private CommonsController commons;
-
 	public JPAUtil() {
 		factory = Persistence.createEntityManagerFactory("salaoBeleza");
 	}
@@ -36,14 +28,6 @@ public class JPAUtil implements Serializable {
 	@Produces
 	@RequestScoped
 	public EntityManager createEntityManager() {
-		try{
-			commons.getUsuario();
-			Map<String, String> properties = new HashMap<>();
-			properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost/salaoBeleza");
-			factory = Persistence.createEntityManagerFactory("salaoBeleza", properties);
-		}catch(Exception e){
-			return factory.createEntityManager();
-		}
 		return factory.createEntityManager();
 	}
 
