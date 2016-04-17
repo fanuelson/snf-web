b240c1f0502cfae92f465a12d4ef63d368d665f7
fanuel nunes <fanuelson@gmail.com>
2016-04-17 19:09:33 -0300

    Colocar Factories RequestScoped.

--- src/main/java/com/snf/factory/CaixaAbertoFactory.java
+++ src/main/java/com/snf/factory/CaixaAbertoFactory.java
@@ -1,12 +1,14 @@
 package com.snf.factory;
 
 import javax.enterprise.inject.Produces;
+import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 
 import com.snf.model.Caixa;
 import com.snf.qualifier.CaixaAberto;
 import com.snf.service.CaixaService;
 
+@RequestScoped
 public class CaixaAbertoFactory {
 	
 	@Inject
