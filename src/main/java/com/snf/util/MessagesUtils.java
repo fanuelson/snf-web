5bfc378a14054a20f2074eeefb2c132e21c793a5
fanuel nunes <fanuelson@gmail.com>
2016-04-24 20:21:19 -0300

    Sistema preparado para Notificações em tempo Real.

--- src/main/java/com/snf/util/MessagesUtils.java
+++ src/main/java/com/snf/util/MessagesUtils.java
@@ -38,15 +38,37 @@ public class MessagesUtils {
 	public static void exibirMensagemErro(String keyMessage, Object... parametros) {
 		exibirMensagem(FacesMessage.SEVERITY_ERROR, keyMessage, parametros);
 	}
+	
+	public static FacesMessage createMensagemSucesso(String keyMessage, Object... parametros) {
+		return createMensagem(FacesMessage.SEVERITY_INFO, keyMessage, parametros);
+	}
+	
+	public static FacesMessage createMensagemAlerta(String keyMessage, Object... parametros) {
+		return createMensagem(FacesMessage.SEVERITY_WARN, keyMessage, parametros);
+	}
+	
+	public static FacesMessage createMensagemErro(String keyMessage, Object... parametros) {
+		return createMensagem(FacesMessage.SEVERITY_ERROR, keyMessage, parametros);
+	}
 
 	public static void exibirMensagem(FacesMessage.Severity severity, String mensagem1, String mensagem2) {
 		FacesMessage facesMessage = new FacesMessage(severity, mensagem1, mensagem2);
 		FacesContext.getCurrentInstance().addMessage(null, facesMessage);
 	}
+	
+	public static FacesMessage createMensagem(FacesMessage.Severity severity, String mensagem1, String mensagem2) {
+		FacesMessage facesMessage = new FacesMessage(severity, mensagem1, mensagem2);
+		return facesMessage;
+	}
 
 	private static void exibirMensagem(FacesMessage.Severity severity, String keyMessage, Object... parametros) {
 		String mensagem = getMessage(keyMessage, parametros);
 		exibirMensagem(severity, mensagem, "");
+	}
+	
+	private static FacesMessage createMensagem(FacesMessage.Severity severity, String keyMessage, Object... parametros) {
+		String mensagem = getMessage(keyMessage, parametros);
+		return createMensagem(severity, mensagem, "");
 	}
 
 }
