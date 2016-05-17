4b8c51bd50f5633faae54d8fe6d82065c379387b
fanuel nunes <fanuelson@gmail.com>
2016-05-17 00:23:04 -0300

    Retirar nome de funcionario. Usuario ja possui tal atributo.

--- src/main/java/com/snf/model/Funcionario.java
+++ src/main/java/com/snf/model/Funcionario.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.inject.Named;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
@@ -18,19 +17,8 @@ public class Funcionario extends Usuario implements Serializable {
 
 	private static final long serialVersionUID = 8422266960584099194L;
 
-	@Column(name = "nome")
-	private String nome;
-
 	@OneToMany(mappedBy = "funcionario", cascade = { CascadeType.PERSIST, CascadeType.MERGE }, orphanRemoval = false)
 	private List<Servico> servicos = new ArrayList<>();
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 
 	public List<Servico> getServicos() {
 		return servicos;
